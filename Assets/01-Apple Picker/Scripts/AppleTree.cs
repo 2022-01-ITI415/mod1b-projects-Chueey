07f4b1a70f9f3600321422d9b7d51de5d993f239
Chuyuan <cc1705@scarletmail.rutgers.edu>
2022-02-02 02:30:59 -0500

    Progress 6

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -13,12 +13,26 @@ public class AppleTree : MonoBehaviour
     public float secondsBetweenAppleDrops = 1f;
     void Start()
     {
-        
+     Invoke( "DropApple", 2f );   
     }
+     void DropApple() {
+           GameObject apple = Instantiate<GameObject
+           apple.transform.position = transform.position
+           Invoke( "DropApple", secondsBetweenAppleDrops
 
     // Update is called once per frame
     void Update()
     {
-        
+      Vector3 pos = transform.position;
+      pos.x += speed * Time.deltTime;
+      transform.position = pos;
+
+      if ( pos.x < -leftAndRightEdge ) {
+        speed = Mathf.Abs(speed) ;
+      } else if ( pos.x > leftAndRightEdge ) {
+          speed = -Mathf.Abs(speed);
+      } else if ( Random.value < chanceToChangeDirections) {
+          speed *= -1;
+      }
     }
 }
