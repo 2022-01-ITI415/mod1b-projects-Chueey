f49d756d394a4f0071dbfddb1c2ba9091137b054
Chuyuan <cc1705@scarletmail.rutgers.edu>
2022-02-02 23:27:55 -0500

    Progress 7

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -2,37 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour
-{
+public class AppleTree : MonoBehaviour {
     [Header("Set in Inspector")]
     // Start is called before the first frame update
     public GameObject applePrefab;
-    public float speed = 1f;
-    public float leftAndRightEdge = 20;
-    public float chanceToChangeDirections = 0.1f;
-    public float secondsBetweenAppleDrops = 1f;
-    void Start()
-    {
-     Invoke( "DropApple", 2f );   
-    }
-     void DropApple() {
-           GameObject apple = Instantiate<GameObject
-           apple.transform.position = transform.position
-           Invoke( "DropApple", secondsBetweenAppleDrops
+    
+    public float    speed = 1f;
+   
+    public float    leftAndRightEdge = 10f;
+    
+    public float    chanceToChangeDirections = 0.1f;
+    
+    public float    secondsBetweenAppleDrops = 1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-      Vector3 pos = transform.position;
-      pos.x += speed * Time.deltTime;
-      transform.position = pos;
-
-      if ( pos.x < -leftAndRightEdge ) {
-        speed = Mathf.Abs(speed) ;
-      } else if ( pos.x > leftAndRightEdge ) {
-          speed = -Mathf.Abs(speed);
-      } else if ( Random.value < chanceToChangeDirections) {
-          speed *= -1;
-      }
-    }
+void Update () {
+    Vector3 pos = transform.position;
+    pos.x += speed * Time.deltaTime;
+    transform.position = pos;
+}
 }
