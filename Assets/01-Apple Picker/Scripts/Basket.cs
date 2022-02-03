523e5bf7ee370dc19a34687347914a5aa75fbccb
Chuyuan <cc1705@scarletmail.rutgers.edu>
2022-02-03 00:35:12 -0500

    Progress 12

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -18,6 +18,13 @@ public class Basket : MonoBehaviour
     Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
     Vector3 pos = this.transform.position;
     pos.x = mousePos3D.x;
-    this.transform.position = pos;   
+    this.transform.position = pos;
+
+    void OnCollisionEnter( Collision coll ) {
+        GameObject collidedWith = coll.gameObject;
+        if ( collidedWith.tag == "Apple" ) {
+            Destroy( collidedWith ) ;
+        }
+    }    
     }
 }
