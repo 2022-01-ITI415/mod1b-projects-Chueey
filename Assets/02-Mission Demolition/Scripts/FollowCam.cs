efe2ee64ad880226ce1a349a07fba1fa51c37b92
Chuyuan <cc1705@scarletmail.rutgers.edu>
2022-02-12 01:10:44 -0500

    Camera follows Projectile

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class FollowCam : MonoBehaviour
 {
     static public GameObject POI;
+    [Header("Set in Inspector")]
+    public float        easing = 0.05f;
+    public Vector2      minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
     public float        camZ;
     
@@ -14,7 +18,11 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return;
 
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max( minXY.x, destination.x);
+        destination.y = Mathf.Max( minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
