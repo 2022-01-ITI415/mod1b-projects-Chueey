e4b6736ce8f5f7cd56dfae4ed85a64546bb67f28
Chuyuan <cc1705@scarletmail.rutgers.edu>
2022-02-11 22:20:37 -0500

    Instantiating a Projectile
    update slingshot.cs
    projectile appears when mouse is clicked

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using System.Collections;
 
-public class Slingshot : MonoBehaviour
-{
+public class Slingshot : MonoBehaviour{
+    [Header("Set in Inspector")]
+    public GameObject       prefabProjectile;
+    [Header("Set Dynamically")]
     public GameObject       launchPoint;
+    public Vector3          launchPos;
+    public GameObject       projectile;
+    public bool             aimingMode;
 
     void Awake()    {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive( false );
+        launchPos = launchPointTrans.position;
     }    
     void OnMouseEnter() {
         //print("Slingshot:OnMouseEnter()");
@@ -17,5 +23,11 @@ public class Slingshot : MonoBehaviour
     void OnMouseExit() {
         //print("Slingshot:OnMouseExit()");
         launchPoint.SetActive( false );
+    }
+    void OnMouseDown() {
+        aimingMode = true;
+        projectile = Instantiate( prefabProjectile ) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
